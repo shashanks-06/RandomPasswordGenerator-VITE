90c2510b7b91656f8a01edb170e14bceb9452cb4
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-06-23 19:31:18 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { FaRegCopy } from "react-icons/fa";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <h1>
+          Generate a <br />
+          Random Password
+        </h1>
+        <div className="display">
+          <input type="password" placeholder="Password" id="password" />
+          <FaRegCopy />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
